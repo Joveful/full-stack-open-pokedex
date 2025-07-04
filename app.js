fd723f478856be400db97b4dd57c44f2d3960d64
Jori Vehkakoski <j.vehkakoski@gmail.com>
2025-07-04 12:09:39 +0300

    disable eslint

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (_req, res) => {
-  // eslint-ignore-next-line no-constant-condition
+  // eslint-disable-next-line no-constant-condition
   if (true) throw ('error...')
   res.send('ok')
 })
